bb5ed312a4699cb022efe68beb85459cd4074b4f
ImRaNM-001 <59203975+ImRaNM-001@users.noreply.github.com>
2022-05-15 22:49:06 +0530

    push-post-install[15-May-2022]

---
+++ test/specs/myFirstTest.ts
@@ -0,0 +1,34 @@
+import {test, expect} from '@playwright/test';
+
+
+test('practice test', async ({page}) =>{
+    await page.locator('jj').fil
+
+   
+
+
+
+    
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
