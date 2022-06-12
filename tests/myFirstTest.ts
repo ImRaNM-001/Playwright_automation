2917642b68eed65cd3fa95a691a804751b2275be
ImRaNM-001 <59203975+ImRaNM-001@users.noreply.github.com>
2022-06-12 08:05:15 +0530

    files-modified-to-Playwright-from-WDIO

---
+++ tests/myFirstTest.ts
@@ -0,0 +1,34 @@
+import {test, expect} from '@playwright/test';
+
+
+test('practice test', async ({page}) =>{
+    await page.locator('jj').filling here       // sample commented 12-Jun-2022
+
+   
+
+
+  
+    
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
