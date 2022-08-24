0ee66b023a68a4f8034221292b9f2ef761e7fd18
ImRaNM-001 <59203975+ImRaNM-001@users.noreply.github.com>
2022-08-24 09:20:35 +0530

    test_Commit-24-Aug-2022

--- tests/myFirstTest.ts
+++ tests/myFirstTest.ts
@@ -2,7 +2,7 @@ import {test, expect} from '@playwright/test';
 
 
 test('practice test', async ({page}) =>{
-    await page.locator('jj').filling here       // sample commented 12-Jun-2022
+    await page.locator('jj').filling here       // sample commented 24-Aug-2022
 
    
 
